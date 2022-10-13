c4ab387f58ff03b0e33304b419055b6270acbfc0
Andrew Brown <drusepth@gmail.com>
2022-10-12 20:07:04 -0700

    spawn ore based on server state

--- Assets/Scripts/Networking/ServerUpdateListener.cs
+++ Assets/Scripts/Networking/ServerUpdateListener.cs
@@ -6,20 +6,19 @@ using UnityEngine;
 public class ServerUpdateListener : Singleton<ServerUpdateListener>
 {
     public Transform player;
-    public string local_player_id = null;
+    public int local_player_id = 0;
 
     public GameObject nearby_player_prefab;
+    public GameObject ore_prefab;
 
     public void Start()
     {
-        local_player_id = player.GetComponent<ServerPositionReporter>().player_id.ToString();
+        local_player_id = player.GetComponent<ServerPositionReporter>().player_id;
     }
 
     public void ProcessNewMessage(string full_message)
     {
-
-        full_message = "{\"characters\":[{\"player_id\":2,\"position\":{\"x\":31.319707165172915,\"y\":72.94538403401285},\"inventory\":{}},{\"player_id\":1,\"position\":{\"x\":1.0,\"y\":1.0},\"inventory\":{}}]}";
-
+        full_message = "{\"characters\":[{\"player_id\":2,\"position\":{\"x\":31.319707165172915,\"y\":72.94538403401285},\"inventory\":{}},{\"player_id\":1,\"position\":{\"x\":1.0,\"y\":1.0},\"inventory\":{}}], \"ore\": [{\"ore_id\":4,\"ore_type\":\"IRON\",\"amount\":2,\"position\":{\"x\":25.804023924743056,\"z\":25.945807143160344}}]}";
         ServerGameState game_state = JsonUtility.FromJson<ServerGameState>(full_message);
 
         #region Example game state API response
@@ -102,35 +101,50 @@ public class ServerUpdateListener : Singleton<ServerUpdateListener>
         // Update nearby players
         foreach (ServerPlayerData player_data in game_state.characters)
         {
-            string player_id = player_data.player_id.ToString();
-
+            int player_id = player_data.player_id;
             if (player_id != local_player_id)
                 UpdateNearbyPlayer(FindOrCreateOtherPlayerById(player_id), player_data);
         }
         
         // Update nearby objects
-        // TODO
+        foreach (ServerOreData ore_data in game_state.ore)
+        {
+            GameObject ore = FindOrCreateObjectById(ore_data.ore_id, ore_prefab);
+            UpdateObjectPosition(ore, ore_data.position);
+        }
     }
 
     #region Helper Functions
-    public GameObject FindOrCreateOtherPlayerById(string player_id)
+    public GameObject FindOrCreateOtherPlayerById(int player_id)
     {
         // Find the GameObject in the OtherPlayersPool with the matching player_id
-        GameObject found_player = null;
         GameObject player_pool = GameObject.Find("OtherPlayersPool");
         foreach (var position_reporter in player_pool.GetComponentsInChildren<ServerPositionReporter>())
-            if (position_reporter.player_id.ToString() == player_id)
+            if (position_reporter.player_id == player_id)
                 return position_reporter.gameObject;
 
         // If the player doesn't exist yet, create a new one
-        if (found_player == null)
-        {
-            found_player = Instantiate(nearby_player_prefab, Vector3.zero, Quaternion.identity);
-            found_player.GetComponent<ServerPositionReporter>().player_id = int.Parse(player_id);
-            found_player.transform.parent = player_pool.transform;
-        }
+        GameObject found_player = Instantiate(nearby_player_prefab, Vector3.zero, Quaternion.identity);
+        found_player.GetComponent<ServerPositionReporter>().player_id = player_id;
+        found_player.transform.parent = player_pool.transform;
 
         return found_player;
+    }
+
+    public GameObject FindOrCreateObjectById(int object_id, GameObject prefab)
+    {
+        // Find the GameObject in the ObjectPool with the matching id
+        GameObject object_pool = GameObject.Find("ObjectPool");
+        foreach (var uoid in object_pool.GetComponentsInChildren<UniqueObjectIdentifier>())
+            if (uoid.uoid == object_id)
+                return uoid.gameObject;
+
+        // If the object doesn't exist yet, create a new one
+        GameObject new_obj = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        new_obj.GetComponent<UniqueObjectIdentifier>().uoid = object_id;
+        new_obj.transform.parent = object_pool.transform;
+
+        return new_obj;
     }
     #endregion
 
